fc18fe2d5ee70146130a6144d57c6912a0bb48f3
Shiv <166595376+Shivsamb-Jalkote@users.noreply.github.com>
2025-10-16 16:14:34 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,35 @@
-variable "aws_region" { type = string, default = "us-east-1" }
-variable "cluster_name" { type = string, default = "default" }
-variable "vpc_id" { type = string }
-variable "subnet_ids" { type = list(string) }
-variable "node_group_desired_capacity" { type = number, default = 2 }
-variable "node_instance_type" { type = string, default = "t3.medium" }
-variable "key_name" { type = string, default = "your-keypair" }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "default"
+}
+
+variable "vpc_id" {
+  description = "Existing VPC ID to use"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs in your VPC"
+  type        = list(string)
+}
+
+variable "node_group_desired_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "t3.medium"
+}
+
+variable "key_name" {
+  description = "EC2 Keypair name to attach to worker nodes"
+  type        = string
+  default     = "keypair"
+}
